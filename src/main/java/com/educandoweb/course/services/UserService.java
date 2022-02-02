037465e75ab28bb70bc82977ab8737b0fec9abc9
Gabriel-R-Santos <gabriel.rds.developer@gmail.com>
2022-02-02 13:07:16 -0300

    Deploy app to Heroku

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -19,27 +22,40 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(long id) {
-		
-		Optional<User> obj = repository.findById(id);		
-		return obj.orElseThrow( () -> new ResourceNotFoundException(id));
-		
+
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			
+			repository.deleteById(id);
+			
+		} catch (EmptyResultDataAccessException e) {
+			
+			throw new ResourceNotFoundException(id);
+			
+		} catch (DataIntegrityViolationException e) {
+			
+			throw new DatabaseException(e.getMessage());
+			
+		}
 	}
 
 	public User update(Long id, User obj) {
-		
-		User entity = repository.getOne(id);
-		updateData(entity,  obj);
+
+		User entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+		updateData(entity, obj);
 		return repository.save(entity);
+
 	}
 
 	private void updateData(User entity, User obj) {
@@ -47,6 +63,6 @@ public class UserService {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
 }
